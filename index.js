2650915951398d6a1fcdbf588d7896edeea2a5fa
Naga Venkata Sai <nagavenkatasai95@gmail.com>
2026-02-01 20:52:53 -0500

    made change in tb2

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ const authenticate = (req, res, next) => {
 //these are the changes that i have made on the test branch
 
 // GET /users?id=101
+// these are the changes im making in master
+// these are the changes im making in testbranch2
 app.get("/users", authenticate, (req, res) => {
   const userId = parseInt(req.query.id);
   const user = users.find((u) => u.id === userId);
